c29d83ba876b4af50267862848ed4715e3af57a0
DanPlayz23 <86156119+DanPlayz23@users.noreply.github.com>
2022-10-15 19:07:31 +0200

    Update arxhub.lua

--- arxhub.lua
+++ arxhub.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 443406476 then
-  loadstring(game:HttpGet(("https://raw.githubusercontent.com/DanPlayz23/ARXHub_PL/main/arxhub.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/DanPlayz23/ARXHub_PL/main/arxhub.lua"))()
 end
