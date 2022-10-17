cd39cb1c9b795ff16a101ed2d0dbbe9b762c2929
Aristois <86156119+DanPlayz23@users.noreply.github.com>
2022-10-17 19:00:17 +0200

    Update arxhub.lua

--- arxhub.lua
+++ arxhub.lua
@@ -1,3 +1,6 @@
 if game.PlaceId == 443406476 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/DanPlayz23/ARXHub_PL/main/arxhub.lua"))()
 end
+if game.PlaceId == 8750997647 then
+  
+end
